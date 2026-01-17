889d2d036a1e236f17f2ba6481007534f28dac17
Bharath552-bit <patthembharath32@gmail.com>
2026-01-17 13:25:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+	const prices=Array.from(document.querySelectorAll(".price")) 
+	const total = prices.reduce((acc, el) => {
+	return acc + (Number(el.textContent) || 0);
+}, 0);
+
+	const table=document.querySelector("table")
+	const newRow=document.createElement("tr")
+	const newData=document.createElement("td")
+	newData.innerText=`${total}`
+	table.appendChild(newRow)
+	newRow.appendChild(newData)
 };
 
 getSumBtn.addEventListener("click", getSum);
